c14323b7cf39534c7e42ad30727ee5428ec3ac87
Nazita470 <nazarenosalvatierra0@gmail.com>
2024-02-09 19:19:34 -0300

    desafio 3 terminado

---
+++ tercer-desafio/app.js
@@ -0,0 +1,25 @@
+import { ProductManager } from "./ProductManager.js"
+import express from "express"
+
+const manager = new ProductManager()
+const products = await manager.getProducts()
+const path = 3000
+
+const app = express()
+app.use(express.urlencoded({extended: true}))
+app.listen(path, () => console.log("Corriendo en el puerto: ", path))
+
+
+app.get("/products", (req, res) => {
+    const query = req.query.limit
+    let newProducts = []
+    query ?  newProducts = products.slice(0, query) : newProducts = [...products]
+    res.send(newProducts)
+})
+
+app.get("/products/:pid", (req, res) => {
+    const param = req.params.pid
+    const prod = products.find((p) => p.id == param)
+    const error = {error: "Producto no encontrado"}
+    prod ? res.send(prod) : res.send(error)
+})
